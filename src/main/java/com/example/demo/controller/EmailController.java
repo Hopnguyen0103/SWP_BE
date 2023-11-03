dd4bbb360a6e9d9b6a1ee44969b803153d8d787d
duyanh2003 <108049889+duyanh2003@users.noreply.github.com>
2023-11-03 13:30:47 +0700

    Update EmailController.java

--- src/main/java/com/example/demo/controller/EmailController.java
+++ src/main/java/com/example/demo/controller/EmailController.java
@@ -7,19 +7,4 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
-public class EmailController {
-    @PostMapping("/sendEmail")
-    public ResponseEntity<Object> sendEmail(@RequestParam int orderId) {
-        try {
-            String body = EmailRepository.messageCreate(orderId);
-            String subject = "Xác nhận đơn hàng mã:" + orderId;
-            String email = OrderDetailsRepository.getOrderDetailsByOrderId(orderId).getUser().getEmail();
-            if(EmailRepository.sendEmail(subject, body, email)){
-                return ResponseEntity.ok().build();
-            }
-            else return ResponseEntity.badRequest().build();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
+
